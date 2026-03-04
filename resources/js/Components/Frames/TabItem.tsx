af30bd4f6f654500622a9fc536f3b4ecb93de039
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2026-03-04 20:48:48 -0300

    add href atribute

--- resources/js/Components/Frames/TabItem.tsx
+++ resources/js/Components/Frames/TabItem.tsx
@@ -2,19 +2,22 @@ import { Link } from '@inertiajs/react';
 
 interface TabProps {
     label: string;
+    href?: string;
     active?: boolean;
 }
 
-export default function SidebarTab({ label, active = false }: TabProps) {
+export default function SidebarTab({ label, href = '#', active = false }: TabProps) {
     return (
-        <a
+        <Link
+            href={href}
             className={`
                 flex items-center gap-3 px-4 py-2.5 w-full transition-all duration-200 group rounded-lg font-medium text-sm
+                ${active ? 'bg-white/15 text-white' : 'text-white/80 hover:text-white hover:bg-white/10'}
             `}
         >
             {/*icon && <span className="text-lg opacity-80 group-hover:opacity-100">{icon}</span>*/}
             
             <span>{label}</span>
-        </a>
+        </Link>
     );
 }
