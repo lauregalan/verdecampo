d26ed4ae6e279b67506591fbd86a6e0424d10090
VelosoMauricio <mauriciojulian545@gmail.com>
2026-03-04 19:44:35 -0300

    estructura del frame actualizada para que puedan trabajar :)

--- resources/js/Pages/Frames/Main.tsx
+++ resources/js/Pages/Frames/Main.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren, ReactNode } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo'; //
-import { Link } from '@inertiajs/react';
+import TabItem from '@/Components/Frames/TabItem';
+
 
 interface AuthenticatedLayoutProps {
     header?: ReactNode;
@@ -15,17 +16,16 @@ export default function AuthenticatedLayout({
         <div>
             
         </div>
-        <div className="m-4 m-10 w-full max-w-7xl h-[90vh] bg-white/50 p-6 rounded-xl shadow-2xl">
+        <div className="m-1 w-full max-w-8xl h-[93vh] bg-white/50 p-1.5 rounded-xl shadow-2xl">
             
             {/* acá arranca la parte de las tabs, la idea es modificar el frame segun los permisos que tenga el usuario */}
-            <div className="w-64 h-screen bg-[#0f2e1e] text-white p-6 flex flex-col shadow-xl">
+            <div className="w-64 h-full rounded-xl rounded-tr-none bg-[#0f2e1e] text-white p-6 flex flex-col shadow-xl">
                 <div className="flex items-center gap-3 mb-10">
                     <ApplicationLogo className="w-8 h-8 fill-white" />
                     <span className="text-xl font-bold tracking-tight">Verdecampo</span>
                 </div>
 
                 <nav className="space-y-2">
-                    {/*
                     <TabItem label="Dashboard" active={false} />
                     <TabItem label="Mis Lotes" active={false} />
                     <TabItem label="Gestión de Personal" active={true} />
@@ -33,7 +33,6 @@ export default function AuthenticatedLayout({
                     <TabItem label="Informes" active={false} />
                     <TabItem label="Configuración" active={false} />
                     <TabItem label="Ayuda" active={false} />
-                    */}
                 </nav>
             </div>
 
