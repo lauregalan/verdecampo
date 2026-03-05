3a4c2c1b702993674c77875709af64df0e92c377
santiagofrances01 <santiagofrances01@gmail.com>
2026-03-05 11:55:09 -0300

    andando

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', function () {
     return response()->json(['message' => 'Hola!']);
 });
+Route::apiResource('campos', App\Http\Controllers\CampoController::class);
