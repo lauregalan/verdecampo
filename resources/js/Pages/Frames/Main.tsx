63f25db753b6be23d8b96ffc4f99b325db95902c
Laureano <lauregalan1@gmail.com>
2026-03-05 11:37:18 -0300

    style: cambiar estilo de las tablas y disposicion

--- resources/js/Pages/Frames/Main.tsx
+++ resources/js/Pages/Frames/Main.tsx
@@ -41,7 +41,28 @@ import {
   InputGroupAddon,
   InputGroupInput,
 } from "@/components/ui/input-group"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 import { Search } from "lucide-react"
+
+import {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Button } from '@/components/ui/button';
+import { Link } from '@inertiajs/react';
+
+import { Bell } from "lucide-react"; 
+import { Badge } from "@/components/ui/badge";
 
  // despues mover a un componente, datos de prueba
 const invoices = [
@@ -99,6 +120,7 @@ export default function AuthenticatedLayout({
     children,
 }: PropsWithChildren<AuthenticatedLayoutProps>) {
     return (
+        
         <SidebarProvider defaultOpen={true}>
             <div className="flex items-center justify-center min-h-screen w-full bg-fondo-agro bg-cover bg-center bg-no-repeat p-4">
                 
@@ -183,66 +205,116 @@ export default function AuthenticatedLayout({
                         </SidebarFooter>
          
                     </Sidebar>
-                    
-                    {/* MAIN CONTENT */}
-                    <main className="flex-1 overflow-y-auto custom-scrollbar p-8">
+                    <div className="flex-1 flex flex-col min-w-0 bg-transparent h-full">
 
-                        {/* tambien moverlo a un componente al buscador */}
+                        <div className="mx-1 bg-transparent h-20 w-full flex items-center px-6 rounded-tr-xl justify-between">
 
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem>
+                                    <BreadcrumbLink asChild>
+                                        <Link href="/">Home</Link>
+                                    </BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator />
+                                    <BreadcrumbItem>
+                                        <Link href="/">Manteca</Link>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator />
+                                    <BreadcrumbItem>
+                                    <BreadcrumbLink asChild>
+                                        <Link href="/">Anashe</Link>
+                                    </BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator />
+                                    <BreadcrumbItem>
+                                    <BreadcrumbPage>Alonso</BreadcrumbPage>
+                                    </BreadcrumbItem>
+                                </BreadcrumbList>
+                            </Breadcrumb>
 
-                        <InputGroup className="max-w-full">
-                        <InputGroupInput placeholder="Search..." />
-                        <InputGroupAddon>
-                            <Search />
-                        </InputGroupAddon>
-                        <InputGroupAddon align="inline-end">12 results</InputGroupAddon>
-                        </InputGroup>
-                        {/* TRANSFORMARLA EN COMPONENTE DESP. ASI QUEDA MAS CLEAN*/}
-                        <Table>
-                            <TableCaption>A list of your recent invoices.</TableCaption>
-                            <TableHeader>
-                                <TableRow>
-                                <TableHead className="w-[300px]">Nombre y apellido</TableHead>
-                                <TableHead>Correo electronico</TableHead>
-                                <TableHead>Rol</TableHead>
-                                <TableHead className="text-right">Estado</TableHead>
-                                <TableHead className="text-right">Ultimo acceso</TableHead>
-                                <TableHead className="text-right">Acciones</TableHead>
+                            <div className="flex flex-row gap-5">
+                
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                            </div>
 
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {invoices.map((invoice) => (
-                                <TableRow key={invoice.invoice}>
-                                    <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                                    <TableCell>{invoice.paymentStatus}</TableCell>
-                                    <TableCell>{invoice.paymentMethod}</TableCell>
-                                    <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-                                </TableRow>
-                                ))}
-                            </TableBody>
-                            <TableFooter>
-                                <TableRow>
-                                <TableCell colSpan={3}>Total</TableCell>
-                                <TableCell className="text-right">$2,500.00</TableCell>
-                                </TableRow>
-                            </TableFooter>
-                        </Table>
-                        {header && (
-                            <header className="mb-8 border-b border-black/5 pb-4">
-                                <h1 className="text-2xl font-black text-[#0f2e1e] uppercase tracking-tight">
-                                    {header}
-                                </h1>
-                            </header>
-                        )}
-                        
-                        <div className="w-full">
-                            {children}
                         </div>
-                    </main>
-                    
+
+                        <div className="mx-1 w-full h-1 bg-lime-800">
+
+                        </div>
+                        
+                    {/* MAIN CONTENT */}
+                        <main className="flex-1 overflow-y-auto custom-scrollbar p-8">
+
+
+                            {/* tambien moverlo a un componente al buscador */}
+                            <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                                Gestion de usuarios
+                            </h2>
+
+                            <InputGroup className="max-w-full bg-white">
+                            <InputGroupInput placeholder="Search..." />
+                            <InputGroupAddon>
+                                <Search />
+                            </InputGroupAddon>
+                            <InputGroupAddon align="inline-end">12 results</InputGroupAddon>
+                            </InputGroup>
+                            {/* TRANSFORMARLA EN COMPONENTE DESP. ASI QUEDA MAS CLEAN*/}
+                            <Table>
+                                <TableCaption>A list of your recent invoices.</TableCaption>
+                                <TableHeader>
+                                    <TableRow>
+                                    <TableHead className="w-[300px]">Nombre y apellido</TableHead>
+                                    <TableHead>Correo electronico</TableHead>
+                                    <TableHead>Rol</TableHead>
+                                    <TableHead className="text-right">Estado</TableHead>
+                                    <TableHead className="text-right">Ultimo acceso</TableHead>
+                                    <TableHead className="text-right">Acciones</TableHead>
+
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {invoices.map((invoice) => (
+                                    <TableRow key={invoice.invoice}>
+                                        <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                                        <TableCell>{invoice.paymentStatus}</TableCell>
+                                        <TableCell>{invoice.paymentMethod}</TableCell>
+                                        <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                                    </TableRow>
+                                    ))}
+                                </TableBody>
+                                <TableFooter>
+                                    <TableRow>
+                                    <TableCell colSpan={3}>Total</TableCell>
+                                    <TableCell className="text-right">$2,500.00</TableCell>
+                                    </TableRow>
+                                </TableFooter>
+                            </Table>
+                            {header && (
+                                <header className="mb-8 border-b border-black/5 pb-4">
+                                    <h1 className="text-2xl font-black text-[#0f2e1e] uppercase tracking-tight">
+                                        {header}
+                                    </h1>
+                                </header>
+                            )}
+                            
+                            <div className="w-full">
+                                {children}
+                            </div>
+                        </main>
+                        
+                    </div>
+
+
                 </div>
+                
             </div>
+            
         </SidebarProvider>
+        
     );
 }
