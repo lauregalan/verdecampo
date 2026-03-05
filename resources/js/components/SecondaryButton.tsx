5bcf86068f30f44446e0033a9155a37f413d208a
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2026-03-05 08:46:26 -0300

    fix deleted components 2

--- resources/js/components/SecondaryButton.tsx
+++ resources/js/components/SecondaryButton.tsx
@@ -1,6 +1,7 @@
 import { ButtonHTMLAttributes } from 'react';
 
-export default function PrimaryButton({
+export default function SecondaryButton({
+    type = 'button',
     className = '',
     disabled,
     children,
@@ -9,8 +10,9 @@ export default function PrimaryButton({
     return (
         <button
             {...props}
+            type={type}
             className={
-                `inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${
+                `inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-700 shadow-sm transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 ${
                     disabled && 'opacity-25'
                 } ` + className
             }
