6c9541a4d4ca6ceaee66e9968cd622e6d90e1e54
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2026-03-05 10:03:50 -0300

    add campo scrollarea

--- resources/js/Pages/Campos/Campo.tsx
+++ resources/js/Pages/Campos/Campo.tsx
@@ -1,4 +1,5 @@
 import Main from "@/Pages/Frames/Main";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Head } from "@inertiajs/react";
 import { Eye, MapPin, Pencil, Plus } from "lucide-react";
 
@@ -31,6 +32,24 @@ const campos: Card[] = [
         statusColor: "naranja",
         imageUrl:
             "https://images.unsplash.com/photo-1500382017468-9049fed747ef?auto=format&fit=crop&q=80&w=400",
+    },
+    {
+        name: "El Retiro - Lote 3",
+        surface: "150 Ha",
+        status: "En Barbecho",
+        lastCrop: "Girasol",
+        statusColor: "violeta",
+        imageUrl:
+            "https://images.unsplash.com/photo-1464226184884-fa280b87c399?auto=format&fit=crop&q=80&w=500",
+    },
+    {
+        name: "El Retiro - Lote 3",
+        surface: "150 Ha",
+        status: "En Barbecho",
+        lastCrop: "Girasol",
+        statusColor: "violeta",
+        imageUrl:
+            "https://images.unsplash.com/photo-1464226184884-fa280b87c399?auto=format&fit=crop&q=80&w=500",
     },
     {
         name: "El Retiro - Lote 3",
@@ -114,11 +133,13 @@ export default function Campo() {
                     </button>
                 </div>
 
-                <div className="mx-auto grid max-w-7xl grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-                    {campos.map((campo) => (
-                        <FieldCard key={campo.name} {...campo} />
-                    ))}
-                </div>
+                <ScrollArea className="mx-auto h-[60vh] w-full max-w-7xl rounded-xl pr-4 md:h-[68vh]">
+                    <div className="grid grid-cols-1 gap-8 pb-4 md:grid-cols-2 lg:grid-cols-3">
+                        {campos.map((campo) => (
+                            <FieldCard key={campo.name} {...campo} />
+                        ))}
+                    </div>
+                </ScrollArea>
             </div>
         </Main>
     );
