473e683f3db6b33af86b53589a4b609f36feb819
Coral Milanese <coralmilanese@gmail.com>
2026-03-06 18:01:21 -0300

    Roles a usuarios

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'Hola!']);
 });
 
-Route::get('/roles', [App\Http\Controllers\RolesController::class, 'index']);
+Route::get('/roles', [RoleController::class, 'index']);
+
+Route::put('/users/{id}/roles', [UserController::class, 'modificarRoles']);
