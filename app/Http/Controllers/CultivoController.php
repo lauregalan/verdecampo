d8414890af4789101c194d4f18fed34121ce842e
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2026-04-26 14:51:24 -0300

    get cultivos por lote

--- app/Http/Controllers/CultivoController.php
+++ app/Http/Controllers/CultivoController.php
@@ -41,4 +41,9 @@ class CultivoController extends Controller
 
         return response()->json(null, 204);
     }
+
+    public function getByLote(int $loteId): JsonResponse
+    {
+        return response()->json($this->cultivoService->getByLote($loteId));
+    }
 }
