ae36e53f8027661be720eb31fbd0b91f6ec406cf
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2026-05-25 23:48:45 -0300

    Add factory to models

--- app/Models/Campania.php
+++ app/Models/Campania.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Campania extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'campo_id',
         'nombre',
--- app/Models/Campo.php
+++ app/Models/Campo.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Campo extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['nombre', 'latitud', 'longitud', 'hectareas'];
 
     public function campanias(): HasMany
